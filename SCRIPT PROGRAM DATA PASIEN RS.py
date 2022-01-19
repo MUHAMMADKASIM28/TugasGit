7e4da3077a1e3aae9db7ca931dcc632246cc2dbf
MUHAMMAD KASIM <MUHAMMADKASIM28>
2022-01-19 12:02:54 +0800

    menambah kode program tampilan

---
+++ SCRIPT PROGRAM DATA PASIEN RS.py
@@ -0,0 +1,12 @@
+datas = []
+inputan = 0
+
+while(inputan != "4"):
+   print("## RS. Medika Indonesia ##")
+   print("1. Tampilkan Pasien")
+   print("2. Tambah Pasien")
+   print("3. Hapus Pasien")
+   print("4. Keluar")
+   print("#################")
+   print("Pilih Menu...")
+   inputan = input()
